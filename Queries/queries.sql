6f6d7585df9e43395c838920ccf3fcc5c2d5591b
Megan Fink <finkers13.com>
2021-11-10 23:54:46 -0600

    completed module

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,3 +1,65 @@
+
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (salary, emp_no)
+);
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_employees (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR (4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+
+DROP TABLE titles CASCADE
+
+SELECT * FROM titles
+
+
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
@@ -45,6 +107,9 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+
+DROP TABLE retirement_info;
 
 -- Create new table for retiring employees
 SELECT emp_no, first_name, last_name
@@ -113,6 +178,7 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
+----------------SAVED---------------------------
 --get salaries and put in descending order
 SELECT * FROM salaries
 ORDER BY to_date DESC;
@@ -166,9 +232,36 @@ SELECT ce.emp_no,
 ce.first_name,
 ce.last_name,
 d.dept_name
--- INTO dept_info
+INTO dept_info
 FROM current_emp as ce
 INNER JOIN dept_employees AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+--Create a query that will return only 
+-- the information relevant to the Sales team
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+di.dept_name
+INTO sales_dept
+FROM retirement_info as ri
+LEFT JOIN dept_info as di
+ON ri.emp_no = di.emp_no
+WHERE (di.dept_name = 'Sales')
+
+SELECT * FROM sales_dept
+
+--Create another query that will return the following information for the Sales and Development teams
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+di.dept_name
+INTO sales_development_depts
+FROM retirement_info as ri
+LEFT JOIN dept_info as di
+ON ri.emp_no = di.emp_no
+WHERE di.dept_name IN ('Sales', 'Development')
+
+SELECT * FROM sales_development_depts
