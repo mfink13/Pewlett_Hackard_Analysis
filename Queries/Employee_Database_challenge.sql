f87a64379054c34866e97dd604e131dc811b6ef6
Megan Fink <finkers13.com>
2021-11-14 18:30:59 -0600

    deliverable one complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -14,3 +14,26 @@ ORDER BY e.emp_no
 
 -- Check table --
 SELECT * FROM retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no)
+rt.emp_no,
+    rt.first_name,
+rt.last_name,
+   rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+
+-- Check table
+SELECT * FROM unique_titles
+
+-- Create table with counts per title
+SELECT title, COUNT (title)
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title 
+ORDER BY count DESC
+
+-- Check table
+SELECT * FROM retiring_titles
